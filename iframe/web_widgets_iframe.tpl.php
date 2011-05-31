2f1b100748734e07ccfb5b26bb49e35780987611
Lev Tsypin <lev@thinkshout.com>
2011-05-31 12:48:12 -0700

    Removing some cruft and adding semicolon to style lines.

--- iframe/web_widgets_iframe.tpl.php
+++ iframe/web_widgets_iframe.tpl.php
@@ -1,5 +1,4 @@
 <?php
-// $Id: web_widgets_iframe.tpl.php,v 1.1 2009/06/24 07:38:58 aronnovak Exp $
 /**
  * @file
  * Template for the code what to embed in external sites
--- iframe/web_widgets_iframe_wrapper.tpl.php
+++ iframe/web_widgets_iframe_wrapper.tpl.php
@@ -1,10 +1,10 @@
 <?php
-// $Id: web_widgets_iframe_wrapper.tpl.php,v 1.1.2.2 2010/08/01 02:27:47 diggersf Exp $
 /**
  * @file
  * HTML wrapper code for the iframe'ed code at the external site.
  */
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
